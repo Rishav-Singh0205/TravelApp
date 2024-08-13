4003b9dd775dfdcabe1747f41b0c5e3ff7d65ecd
Rishav Singh <89840621+Rishav-Singh0205@users.noreply.github.com>
2024-08-14 03:13:18 +0530

    my changes

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -3,6 +3,10 @@ const User = require("../modal/user.modal")
 
 
 const sighupController = async (req, res) => {
+    const { username, number, email } = req.body;
+    if (!username || !number || !email || !password) {
+        res.status(401).json("All Inputs is required")
+    }
     try {
         const newUser = new User({
             username: req.body.username,
@@ -10,6 +14,9 @@ const sighupController = async (req, res) => {
             email: req.body.email,
             password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET_KEY).toString()
         });
+
+
+
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (err) {
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const CryptoJS = require('crypto-js');
-const User = require("../modal/user.modal")
-const jwt = require('jsonwebtoken');
+
 
 const signupHandler = require("../controllers/signupController")
 const loginHandler = require("../controllers/loginController")
